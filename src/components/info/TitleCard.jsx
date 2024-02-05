3d7a704954208023fff09f310dacee4a815c4b7b
Greg King <gking8189@gmail.com>
2024-02-05 15:03:49 -0500

    Fixed padding and text size for smaller displays

--- src/components/info/TitleCard.jsx
+++ src/components/info/TitleCard.jsx
@@ -3,13 +3,17 @@ import profilePicture from "../../media/profile-picture.png";
 const TitleCard = () => {
   return (
     <div className="flex flex-row space-x-4 items-center">
-      <div className="w-32 h-32">
+      <div className="w-20 h-20 xl:w-32 xl:h-32">
         <img src={profilePicture} alt="Greg King" className="rounded-full" />
       </div>
       <div>
-        <h1 className="font-bold text-3xl text-gray-100">Greg King</h1>
-        <h2 className="text-lg text-gray-300">Full Stack Software Developer</h2>
-        <p className="text-md text-gray-400">Pittsburgh, PA</p>
+        <h1 className="font-bold text-2xl text-gray-100 xl:text-3xl">
+          Greg King
+        </h1>
+        <h2 className="text-md text-gray-300 xl:text-lg">
+          Full Stack Software Developer
+        </h2>
+        <p className="text-sm text-gray-400 xl:text-md">Pittsburgh, PA</p>
       </div>
     </div>
   );
