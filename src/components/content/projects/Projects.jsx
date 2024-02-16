f83c78501813d7e4da58e80c01187fe5a08850ab
Greg King <gking8189@gmail.com>
2024-02-16 15:39:40 -0500

    Removed View All button from Projects component, WIP

--- src/components/content/projects/Projects.jsx
+++ src/components/content/projects/Projects.jsx
@@ -1,23 +1,11 @@
 import ProjectsContainer from "./ProjectsContainer";
 import { projects } from "../../../data/projects";
 import ProjectCard from "./ProjectCard";
-import Button from "../../layout/Button";
-import { contentSections } from "../../../lib/constants";
 
-const Projects = (props) => {
-  const { modalSectionHandler, openModalHandler } = props;
-
-  const showProjectsModal = () => {
-    modalSectionHandler(contentSections.PROJECTS);
-    openModalHandler(true);
-  };
-
+const Projects = () => {
   return (
     <ProjectsContainer>
-      <div className="flex flex-row justify-between">
-        <h2 className="font-bold text-2xl text-gray-100">Projects</h2>
-        <Button onClick={showProjectsModal}>View All</Button>
-      </div>
+      <h2 className="font-bold text-2xl text-gray-100">Projects</h2>
       {projects &&
         projects.map((p, index) => {
           return <ProjectCard key={index} project={p} />;
